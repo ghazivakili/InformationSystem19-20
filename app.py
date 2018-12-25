c57739f14ab308fe2f8d0492997b1cc15c0da67d
m.ghazivakili <m.ghazivakili@gmail.com>
2018-12-25 13:13:06 +0100

    22 Dec. 2018 : This commit is working with mail and Upload Images.

--- app.py
+++ app.py
@@ -1,12 +1,59 @@
+import os
 from flask import Flask ,render_template , url_for,flash,redirect
 from flask import request,session
-from forms import Formname,LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_mail import Mail, Message
+from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
+
 
 
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'sdfghjkllkjhgfdfghjkkjhgfddfgytrk'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///website.db'
+app.config['UPLOADED_PHOTOS_DEST'] = os.getcwd()+"/photos"
+app.config['MAIL_SERVER']='smtp.mail.com'
+app.config['MAIL_PORT']=587
+app.config['MAIL_TLS']=True
+app.config['MAIL_USERNAME']=os.environ['EMAIL_USERNAME']
+app.config['MAIL_PASSWORD']=os.environ['EMAIL_PASSWORD']
+
+
+db=SQLAlchemy(app)
+bcrypt=Bcrypt(app)
+mailobject=Mail(app)
+
+photos = UploadSet('photos', IMAGES)
+configure_uploads(app, photos)
+patch_request_class(app)  # set maximum file size, default is 16MB
+
+
+
+class Student(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    student_no=db.Column(db.String(20),unique=True,nullable=True)
+    email=db.Column(db.String(50),unique=True,nullable=True)
+    name=db.Column(db.String(100),nullable=True)
+    password=db.Column(db.String(100),nullable=True)
+    role_id=db.Column(db.Integer,db.ForeignKey('role.id'))
+
+    def __repr__(self):
+        return "<Student %r>" % self.name
+
+
+class Role(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(10),nullable=True)
+    students=db.relationship('Student',backref='role',lazy=True)
+
+    def __repr__(self):
+        return "<Role %r>" % self.name
+
+
+from forms import Formname,LoginForm,UploadForm
+
 
 
 #dictionary
@@ -31,6 +78,19 @@ posts = [
     }
 ]
 
+def send_mail(to,subject,template,**kwargs):
+    msg=Message(subject,recipients=[to],sender=app.config['MAIL_USERNAME'])
+    msg.body= render_template(template + '.txt',**kwargs)
+    msg.html= render_template(template + '.html',**kwargs)
+    mailobject.send(msg)
+
+
+
+@app.route('/mail')
+def mail():
+    send_mail('m.ghazivakili@gmail.com','Test message','mail',message_body='Hi this is a test')
+    return 'message has beed send!'
+
 @app.route('/')
 @app.route('/home')
 def home():
@@ -45,6 +105,14 @@ def about():
 def register():
     formpage=Formname()
     if formpage.validate_on_submit():
+        password_1=bcrypt.generate_password_hash(formpage.password.data).encode('utf-8')
+        reg=Student(name=formpage.name.data,
+                email=formpage.email.data,
+                student_no=formpage.student_no.data,
+                password=password_1,
+                role_id=1) #role=Role.query.filter_by(name='Student'))
+        db.session.add(reg)
+        db.session.commit()
         return redirect(url_for('home'))
     return render_template('register.html', formpage = formpage , title='Register Page')
 
@@ -54,14 +122,35 @@ def login():
     formpage=LoginForm()
     if formpage.validate_on_submit():
         # TODO do query db for login or use login from flask
-        session['email']=formpage.email.data
-    return render_template('login.html', formpage = formpage, email=session.get('email',False) , title='Login Page')
+        st=Student.query.filter_by(email=formpage.email.data).first()
+        if st and bcrypt.check_password_hash(st.password,formpage.password.data):
+            session['email']=st.name
+    return render_template('login.html', formpage = formpage,
+                           email=session.get('email',False) ,
+                           title='Login Page')
 
 
-@app.route('/bro')
-def bro():
-    user_agent = request.headers.get('User-Agent')
-    return '<p>Your browser is %s</p>' % user_agent
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('home'))
+
+
+@app.before_first_request
+def setup_all():
+    db.create_all()
+
+@app.route("/fillrole")
+def filldata():
+    db.session.add(Role(name="Student"))
+    db.session.commit()
+
+
+@app.route("/profile")
+def profiles():
+    st=Student.query.filter_by(email="ghazivakili55@gmail.com").all()
+    return render_template("profile.html",students=st)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -71,6 +160,37 @@ def page_not_found(e):
 def internal_error(e):
     return render_template('500.html'), 500
 
+
+
+@app.route("/upload",methods=["POST","GET"])
+def upload():
+    formupload=UploadForm()
+    if formupload.validate_on_submit():
+        filename = photos.save(formupload.file.data)
+        file_url = photos.url(filename)
+        return redirect(file_url)
+    return render_template("upload.html",formupload=formupload)
+
+#
+#
+# def send_mail(to, subject, template, **kwargs):
+#     msg=Message(subject,
+#                 sender=app.config['MAIL_USERNAME'],
+#                 recipients=[to])
+#     # msg.body=render_template(template+'.txt',**kwargs)
+#     msg.html=render_template(template+'.html',**kwargs)
+#     mail.send(msg)
+#
+#
+#
+# @app.route("/mail")
+# def testmail():
+#
+#     send_mail('m.ghazivakili@gmail.com',
+#               'Hi I am test mail'
+#               ,'mail',user='mohammad')
+#
+#     return 'mail has been send'
 
 if __name__ == '__main__':
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField,PasswordField
+from wtforms import StringField,SubmitField,PasswordField,ValidationError,FileField
 from wtforms.validators import Length,Email,EqualTo,DataRequired,regexp
-
+from app import Student,Role
 
 class Formname(FlaskForm):
     name = StringField('Name:', validators=[DataRequired(), Length(min=2, max=20)])
@@ -11,8 +11,24 @@ class Formname(FlaskForm):
     password_con = PasswordField('Confirm', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Submit')
 
+    def validate_student_no(self,student_no):
+        st = Student.query.filter_by(student_no=student_no.data).first()
+        if st:
+            raise ValidationError('You have been registerd or ...')
+
+    def validate_email(self,email):
+        st = Student.query.filter_by(email=email.data).first()
+        if st:
+            raise ValidationError('You have been registerd with email or ...')
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email(),Length(min=4,max=50)])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+
+
+
+class UploadForm(FlaskForm):
+    file = FileField('file',validators=[DataRequired()])
+    upload=SubmitField('upload')
